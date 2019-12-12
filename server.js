c93a691c5c7907799ad250ed33a8b50c9f79f075
shubhamitra-banerjee <58424011+shubhamitra-banerjee@users.noreply.github.com>
2019-12-12 11:42:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ require('dotenv').config({ silent: true });
 
 const app = require('./app.js');
 
-const port = process.env.PORT || 3000;
-app.listen(port);
-console.log('listening at:', port);
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
+//const port = process.env.PORT || 3000;
+app.listen(port,ip);
+//console.log('listening at:', port);
